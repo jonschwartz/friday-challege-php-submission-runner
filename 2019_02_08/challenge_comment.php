b15cf30f00fd624555c3d9ac6f00626e5cd5c6cd
Jon Schwartz <joschwartz@wayfair.com>
2019-02-08 15:24:57 -0500

    added programmatic posting script

---
+++ 2019_02_08/challenge_comment.php
@@ -0,0 +1,30 @@
+/**
+* The elephpants are getting ready to run a meetup of BostonPHP
+* (hosted monthly here at Wayfair, hit up @bcahill for more info)
+* and they need to provide a list of people coming from outside
+* the company to the security desk in the sky lobby.
+*
+* People have RSVP'd on Meetup.com.  When the list of people
+* was printed out, it was in {First Name} {Last Name} Order (mostly).
+*
+* Security has requested the list to be in {Last Name}, {First Name} order.
+* (e.g. Kevin Smith -> Smith, Kevin)
+*
+* Your challenge this week is to take the list as it was provided
+* from meetup.com and translate it to what Security has requested.
+*
+* p.s. Yes we may use this code next time this happens in real life.
+*
+* Caveats:
+*  - first names may be more than one actual name
+*  - For these purposes, last names will only be one name or be hyphenated.
+*  - names may be abbreviated
+*  - People may have submitted their title in front of their name, feel free to drop those
+*  - There will be at least one first name and at least one last name for each person
+*
+* Bonus points:
+*  - your code can handle multi name first names AND multi name last names.
+*  - you alphabetize the list of names by last name
+*
+* Good Luck!
+*/
---
+++ challenge-data/templates/post_challenge_writeup.md
@@ -0,0 +1,1 @@
+This challenge has been posted by Challenge Bot Poster 5000.  I'm a robot.  Beep Boop.  Good Luck!
